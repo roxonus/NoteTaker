89f5bb83fcd9b0251f5f3037dd8bd90c7d7d5326
Douglas Mitchell <roxonus@gmail.com>
2020-09-08 20:11:58 -0700

    fixed api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,51 +1,43 @@
-const db = require("../db/db.json");
+  
+const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
-const uuid = require("uuid/v4");
 
-module.exports = function(app) {
-  app.get("/api/notes", function(req, res) {
-    res.send(db);
-  });
+module.exports = function (app) {
+	app.get("/api/notes", function (req, res) {
+		fs.readFile("db/db.json", "utf8", function (error, data) {
+			res.json(JSON.parse(data));
+		});
+	});
 
-  app.post("/api/notes", function(req, res) {
+	app.post("/api/notes", function (req, res) {
+		var newNote = req.body;
+		newNote.id = uuidv4();
+		fs.readFile("db/db.json", "utf8", function (error, data) {
+			var data = JSON.parse(data);
+			data.push(newNote);
+			fs.writeFile("db/db.json", JSON.stringify(data), function (error) {
+				if (error) throw error;
+				console.log("Written Successfully");
+			});
+		});
+		res.json(newNote);
+	});
 
-    let noteId = uuid();
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text
-    };
-
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-
-      const allNotes = JSON.parse(data);
-
-      allNotes.push(newNote);
-
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
-        if (err) throw err;
-        res.send(db);
-        console.log("Note created!")
-      });
-    });
-  });
-
-  app.delete("/api/notes/:id", (req, res) => {
-
-    let noteId = req.params.id;
-
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-
-      const allNotes = JSON.parse(data);
-      const newAllNotes = allNotes.filter(note => note.id != noteId);
-
-      fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
-        if (err) throw err;
-        res.send(db);
-        console.log("Note deleted!")
-      });
-    });
-  });
+	app.delete("/api/notes/:id", function (req, res) {
+		fs.readFile("db/db.json", "utf8", function (error, data) {
+			let noteId = req.params.id;
+			let noteData = JSON.parse(data);
+			noteData = noteData.filter(function (note) {
+				if (noteId != note.id) {
+					return true;
+				} else {
+					return false;
+				}
+			});
+			fs.writeFile("db/db.json", JSON.stringify(noteData), function (error) {
+				if (error) throw error;
+				res.end(console.log("Deleted Successfully"));
+			});
+		});
+	});
 };
